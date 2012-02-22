12b10f74d6952e341a0f8be0ab6663026c0852de
Alexander Todorov <atodorov@redhat.com>
2012-02-22 16:14:09 +0200

    initial import of plugins

---
+++ bugzilla_plugin.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+
+# Copyright (c) 2012, Alexander Todorov <atodorov()otb.bg>
+
+import os
+import sys
+import getpass
+import bugzilla
+import ConfigParser
+
+def get_config(filename):
+    cp = ConfigParser.SafeConfigParser()
+    cp.read([filename])
+
+    conf = dict(
+            [(key, cp.get('redhat', key)) for key in [
+                'url', 'username'
+            ]]
+        )
+
+    try:
+        conf['password'] = cp.get('redhat', 'password')
+    except ConfigParser.NoOptionError:
+        conf['password'] = getpass.getpass('Password for %s: ' % conf['username'])
+
+    return conf
+
+def get_metrics(start, end, config = None):
+    metrics = {}
+
+    if config is None:
+        config = get_config(os.environ['HOME'] + '/.qe-metrics/bugzilla.conf')
+
+    user = config['username']
+    password = config['password']
+
+    bz = bugzilla.Bugzilla(url='https://%s/xmlrpc.cgi' % config['url'])
+    if not bz.login(user, password):
+        print 'Login failed.'
+        sys.exit(1)
+
+    ##### NEW bugs opened during period
+    qd = {
+        'reporter' : user,
+        'chfieldfrom' : start,
+        'chfieldto' : end,
+        'chfield' : '[Bug creation]',
+    }
+
+    bugs = bz.query(qd)
+
+    metrics['opened'] = len(bugs)
+    #metrics['opened_ids'] = [b.id for b in bugs]
+
+
+    ##### Bugs VERIFIED during period
+
+    qd = {
+        'field0-0-0' : 'bug_status',
+        'type0-0-0' : 'changedto',
+        'value0-0-0' : 'VERIFIED',
+
+        'field0-1-0' : 'bug_status',
+        'type0-1-0' : 'changedby',
+        'value0-1-0' : user,
+
+        'field0-2-0' : 'bug_status',
+        'type0-2-0' : 'changedafter',
+        'value0-2-0' : start,
+
+        'field0-3-0' : 'bug_status',
+        'type0-3-0' : 'changedbefore',
+        'value0-3-0' : end,
+    }
+
+    bugs = bz.query(qd)
+
+    metrics['verified'] = len(bugs)
+    #metrics['verified_ids'] = [b.id for b in bugs]
+
+    ##### Bugs VERIFIED SanityOnly during period
+
+    qd = {
+        'field0-0-0' : 'cf_verified',
+        'type0-0-0' : 'substring',
+        'value0-0-0' : 'SanityOnly',
+
+        'field0-1-0' : 'cf_verified',
+        'type0-1-0' : 'changedby',
+        'value0-1-0' : user,
+
+        'field0-2-0' : 'cf_verified',
+        'type0-2-0' : 'changedafter',
+        'value0-2-0' : start,
+
+        'field0-3-0' : 'cf_verified',
+        'type0-3-0' : 'changedbefore',
+        'value0-3-0' : end,
+    }
+
+    bugs = bz.query(qd)
+
+    metrics['verified_sanity_only'] = len(bugs)
+    #metrics['verified_sanity_only_ids'] = [b.id for b in bugs]
+
+
+    ##### Bugs moved back to ASSIGNED during period
+
+    qd = {
+        'field0-0-0' : 'bug_status',
+        'type0-0-0' : 'changedto',
+        'value0-0-0' : 'ASSIGNED',
+
+        'field0-1-0' : 'bug_status',
+        'type0-1-0' : 'changedby',
+        'value0-1-0' : user,
+
+        'field0-2-0' : 'bug_status',
+        'type0-2-0' : 'changedafter',
+        'value0-2-0' : start,
+
+        'field0-3-0' : 'bug_status',
+        'type0-3-0' : 'changedbefore',
+        'value0-3-0' : end,
+    }
+
+    bugs = bz.query(qd)
+
+    metrics['assigned'] = len(bugs)
+    #metrics['assigned_ids'] = [b.id for b in bugs]
+
+
+    ##### Bugs qa_ack+ during period
+
+    qd = {
+        'field0-0-0' : 'setters.login_name',
+        'type0-0-0' : 'equals',
+        'value0-0-0' : user,
+
+        'field0-1-0' : 'flagtypes.name',
+        'type0-1-0' : 'changedto',
+        'value0-1-0' : 'qa_ack+',
+
+        'field0-2-0' : 'flagtypes.name',
+        'type0-2-0' : 'changedafter',
+        'value0-2-0' : start,
+
+        'field0-3-0' : 'flagtypes.name',
+        'type0-3-0' : 'changedbefore',
+        'value0-3-0' : end,
+    }
+
+    bugs = bz.query(qd)
+
+    metrics['qa_ack+'] = len(bugs)
+    #metrics['qa-ack+_ids'] = [b.id for b in bugs]
+
+    return metrics
+
+
+if __name__ == "__main__":
+    import os
+    import optparse
+    from pprint import pprint
+
+    p = optparse.OptionParser()
+    p.add_option('-s', '--start', dest="start", default=None, help="Start date: YYYY-MM-DD")
+    p.add_option('-e', '--end',   dest="end",   default=None, help="End date:   YYYY-MM-DD")
+
+    (opt, args) = p.parse_args()
+
+    if not opt.start:
+        print "--start is mandatory!"
+        sys.exit(1)
+
+    if not opt.end:
+        print "--end is mandatory!"
+        sys.exit(1)
+
+    opt.start = '%s 00:00:00' % opt.start
+    opt.end = '%s 23:59:59' % opt.end
+
+    metrics =  {
+        'start' : opt.start,
+        'end'   : opt.end,
+        'bugzilla' : get_metrics(opt.start, opt.end),
+    }
+
+    pprint(metrics)
---
+++ request_tracker_plugin.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+
+###########################
+#
+# http://requesttracker.wikia.com/wiki/REST
+#
+###########################
+
+import os
+import pycurl
+import urllib
+import getpass
+import ConfigParser
+
+def get_config(filename):
+    cp = ConfigParser.SafeConfigParser()
+    cp.read([filename])
+
+    conf = dict(
+            [(key, cp.get('main', key)) for key in [
+                'url', 'email'
+            ]]
+        )
+
+    conf['krb_auth'] = cp.get('main', 'krb_auth')
+
+    if not conf['krb_auth']:
+        try:
+            conf['password'] = cp.get('main', 'password')
+        except ConfigParser.NoOptionError:
+            conf['password'] = getpass.getpass('Password for %s: ' % conf['username'])
+
+    return conf
+
+
+class Response:
+    def __init__(self):
+        self.reset()
+
+    def write_content(self, buf):
+        self.contents = self.contents + buf
+
+    def reset(self):
+        self.contents = ''
+
+
+def get_metrics(start, end, config = None):
+    metrics = {}
+
+    if config is None:
+        config = get_config(os.environ['HOME'] + '/.qe-metrics/request_tracker.conf')
+
+    user = config['email']
+
+    # here is the RequestTracker URI we try to access
+    base_uri = 'https://%s/REST/1.0/' % config['url']
+
+
+    # setup curl
+    response = Response()
+    c = pycurl.Curl()
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(pycurl.WRITEFUNCTION, response.write_content)
+    c.setopt(pycurl.SSL_VERIFYPEER, 0)
+
+    if config['krb_auth']:
+        c.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_GSSNEGOTIATE)
+        c.setopt(pycurl.USERPWD, '%s:foo' % user)
+    else:
+        password = config['password']
+        c.setopt(pycurl.USERPWD, '%s:%s' % (user, password))
+
+
+
+    # Tickets created during period:
+    url = base_uri + "/search/ticket?format=i&query=" + urllib.quote("Creator='%s' AND Created > '%s' AND Created < '%s'" % (user, start, end))
+    c.setopt(pycurl.URL, str(url))
+    c.perform()
+
+    tickets = response.contents.decode('UTF-8')
+    response.reset()
+    tickets = [t for t in tickets.split("\n") if t.startswith('ticket')]
+    metrics['opened'] = len(tickets)
+
+
+    # Tickets assigned to user and Resolved during period:
+    url = base_uri + "/search/ticket?format=i&query=" + urllib.quote("Owner='%s' AND Resolved > '%s' AND Resolved < '%s'" % (user, start, end))
+    c.setopt(pycurl.URL, str(url))
+    c.perform()
+
+    tickets = response.contents.decode('UTF-8')
+    response.reset()
+    tickets = [t for t in tickets.split("\n") if t.startswith('ticket')]
+    metrics['resolved'] = len(tickets)
+
+#todo: number of comments on tickets (where user is not owner/assignee)
+
+
+    c.close()
+
+    return metrics
+
+
+if __name__ == "__main__":
+    import sys
+    import optparse
+    from pprint import pprint
+
+    p = optparse.OptionParser()
+    p.add_option('-s', '--start', dest="start", default=None, help="Start date: YYYY-MM-DD")
+    p.add_option('-e', '--end',   dest="end",   default=None, help="End date:   YYYY-MM-DD")
+
+    (opt, args) = p.parse_args()
+
+    if not opt.start:
+        print "--start is mandatory!"
+        sys.exit(1)
+
+    if not opt.end:
+        print "--end is mandatory!"
+        sys.exit(1)
+
+    opt.start = '%s 00:00:00' % opt.start
+    opt.end = '%s 23:59:59' % opt.end
+
+    metrics =  {
+        'start' : opt.start,
+        'end'   : opt.end,
+        'rt' : get_metrics(opt.start, opt.end),
+    }
+
+    pprint(metrics)
