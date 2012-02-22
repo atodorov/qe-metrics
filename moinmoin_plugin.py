3498ef626d0b0b276e366c6ad1f8c9d901c7aa5f
Alexander Todorov <atodorov@otb.bg>
2012-02-22 16:30:59 +0200

    add nitrate and moinmoin plugin

---
+++ moinmoin_plugin.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+###########################
+#
+# QE metrics for atodorov
+#
+###########################
+
+import os
+import getpass
+import xmlrpclib
+import ConfigParser
+from datetime import datetime
+
+def get_config(filename):
+    cp = ConfigParser.SafeConfigParser()
+    cp.read([filename])
+
+    conf = dict(
+            [(key, cp.get('main', key)) for key in [
+                'url', 'username'
+            ]]
+        )
+
+# MoinMoin allows anonymous XML-RPC for the stats
+#    try:
+#        conf['password'] = cp.get('main', 'password')
+#    except ConfigParser.NoOptionError:
+#        conf['password'] = getpass.getpass('Password for %s: ' % conf['username'])
+
+    return conf
+
+
+def get_metrics(start, end, config = None):
+    metrics = {}
+
+    if config is None:
+        config = get_config(os.environ['HOME'] + '/.qe-metrics/moinmoin.conf')
+
+    user = config['username']
+    start_date = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    end_date = datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+
+    # NB: Plain http
+    wiki = xmlrpclib.ServerProxy("http://%s/?action=xmlrpc2" % config['url'])
+
+    # this method can be enhanced
+    # http://moinmo.in/FeatureRequests/getRecentChanges_Filters
+
+    changes = wiki.getRecentChanges(start_date)
+    count = 0
+
+    for change in changes:
+        if change['author'] == user and change['lastModified'] < end_date:
+            count += 1
+
+    metrics['edits'] = count
+
+    return metrics
+
+if __name__ == "__main__":
+    import sys
+    import optparse
+    from pprint import pprint
+
+    p = optparse.OptionParser()
+    p.add_option('-s', '--start', dest="start", default=None, help="Start date: YYYY-MM-DD")
+    p.add_option('-e', '--end',   dest="end",   default=None, help="End date:   YYYY-MM-DD")
+
+    (opt, args) = p.parse_args()
+
+    if not opt.start:
+        print "--start is mandatory!"
+        sys.exit(1)
+
+    if not opt.end:
+        print "--end is mandatory!"
+        sys.exit(1)
+
+    opt.start = '%s 00:00:00' % opt.start
+    opt.end = '%s 23:59:59' % opt.end
+
+    metrics =  {
+        'start' : opt.start,
+        'end'   : opt.end,
+        'wiki' : get_metrics(opt.start, opt.end),
+    }
+
+
+    pprint (metrics)
---
+++ nitrate_plugin.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import os
+from nitrate_driver import NitrateXmlrpc
+
+def get_metrics(start, end):
+    metrics = {}
+
+    nitrate = NitrateXmlrpc.from_config(os.environ['HOME'] + '/.qe-metrics/nitrate.conf')
+    user = nitrate.get_me()
+
+
+    ##### Test Plans created during period
+    plans = nitrate.server.TestPlan.filter_count(
+                            {
+                                'author' : user['id'],
+                                'create_date__gte' : start, 
+                                'create_date__lte' : end,
+                            }
+                        )
+
+    metrics['plans'] = plans
+
+    ##### Test Cases created during period
+
+    cases = nitrate.server.TestCase.filter_count(
+                            {
+                                'author' : user['id'],
+                                'create_date__gte' : start, 
+                                'create_date__lte' : end,
+                            }
+                        )
+
+    metrics['cases'] = cases
+
+    ##### Test Runs executed during period
+
+    runs = nitrate.server.TestRun.filter_count(
+                            {
+                                'manager' : user['id'],
+                                'stop_date__gte' : start,
+                                'stop_date__lte' : end,
+                            }
+                        )
+
+    metrics['runs'] = runs
+
+    ##### Test Case Run execution during period
+
+    caseruns = nitrate.server.TestCaseRun.filter_count(
+                            {
+                                'tested_by' : user['id'],
+                                'close_date__gte' : start,
+                                'close_date__lte' : end,
+                            }
+                        )
+
+    metrics['case_runs'] = caseruns
+
+
+    return metrics
+
+
+if __name__ == "__main__":
+    import sys
+    import optparse
+    from pprint import pprint
+
+    p = optparse.OptionParser()
+    p.add_option('-s', '--start', dest="start", default=None, help="Start date: YYYY-MM-DD")
+    p.add_option('-e', '--end',   dest="end",   default=None, help="End date:   YYYY-MM-DD")
+
+    (opt, args) = p.parse_args()
+
+    if not opt.start:
+        print "--start is mandatory!"
+        sys.exit(1)
+
+    if not opt.end:
+        print "--end is mandatory!"
+        sys.exit(1)
+
+    opt.start = '%s 00:00:00' % opt.start
+    opt.end = '%s 23:59:59' % opt.end
+
+    metrics =  {
+        'start' : opt.start,
+        'end'   : opt.end,
+        'nitrate' : get_metrics(opt.start, opt.end),
+    }
+
+    pprint(metrics)
